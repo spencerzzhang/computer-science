4cb5b8a69cbed016ef6ead84b724fdd3573b226d
spencer zhang <spencerzzhang@gmail.com>
2018-06-27 13:33:50 -0400

    Lesson 1
    This is lesson 1 folder with all the labs

---
+++ Lesson 1/Lab01_03.java
@@ -0,0 +1,14 @@
+public class Lab01_03{
+	public static void main(String[] args) {
+	System.out.print("  _  _ _     ___                ___");
+System.out.print(" | || (_)   |_ _|  __ _ _ __   / __|_ __  ___ _ _  __ ___ _ _ ");
+System.out.print(" | __ | |_   | |  / _` | '  \\  \\__ \\ '_ \\/ -_) ' \\/ _/ -_) '_|");
+System.out.print(" |_||_|_( ) |___| \\__,_|_|_|_| |___/ .__/\\___|_||_\\__\\___|_| ");
+System.out.print("        |/                         |_|                        ");
+	}
+}
+
+
+	
+
+	
