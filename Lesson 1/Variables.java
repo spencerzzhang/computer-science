0e705df469c8a2f211106d6b06a1338a63682c43
spencer zhang <spencerzzhang@gmail.com>
2018-07-06 13:25:48 -0400

    Lesson 1

---
+++ Lesson 1/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+  public static void main(String[] args) {
+	String firstName = "Spencer";
+	String lastName = "Zhang";
+	int age = 14;
+	double mpg = 32.4;
+	System.out.println(firstName + lastName);
+	System.out.println(age);
+	System.out.println(mpg);
+	}
+}
---
+++ Lesson 1/math_errors.java
@@ -0,0 +1,9 @@
+public class math_errors {
+	public static void main (String[] args) {
+		double one = 1.0;
+		double three = 3.0;
+		double quotient = one / three;
+		System.out.println(quotient);
+	}
+}
+		
